c4df1defd8499fb2b9b879dd398ec2f07599972d
Sysleec <sysleec@gmail.com>
2024-08-10 23:11:20 +0300

    added go-prompt for cli, updated go version

--- go.mod
+++ go.mod
@@ -1,11 +1,20 @@
 module github.com/Sysleec/Artifacts-client
 
-go 1.22.5
+go 1.22.6
+
+require (
+	github.com/c-bata/go-prompt v0.2.6
+	github.com/rs/zerolog v1.33.0
+	gopkg.in/ini.v1 v1.67.0
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mattn/go-tty v0.0.7 // indirect
+	github.com/pkg/term v1.2.0-beta.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 )
--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -1,61 +1,64 @@
 package repl
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/Sysleec/Artifacts-client/internal/commands"
 	"github.com/Sysleec/Artifacts-client/internal/models"
-	"os"
+	"github.com/c-bata/go-prompt"
 	"strings"
 )
 
 func Run(cfg *models.Config) {
-	scanner := bufio.NewScanner(os.Stdin)
+	p := prompt.New(
+		executor(cfg),
+		emptyCompleter,
+		prompt.OptionPrefix("Artifacts > "),
+		prompt.OptionTitle("Artifacts CLI"),
+	)
+	p.Run()
+}
 
-	for {
-		fmt.Print("Artifacts > ")
-		scanner.Scan()
-		words := scanner.Text()
-		if len(words) == 0 {
-			continue
+func executor(cfg *models.Config) func(string) {
+	return func(input string) {
+		input = strings.TrimSpace(input)
+		if len(input) == 0 {
+			return
 		}
 
-		wordsSl := commFormatter(words)
+		wordsSl := commFormatter(input)
 
 		if len(wordsSl) > 4 {
 			fmt.Println("Too many args")
-			continue
+			return
 		}
 
 		var commandName string
 		var args []string
 
-		// Find the command in the command list
+		// Find command
 		for i := len(wordsSl); i > 0; i-- {
 			commandName = strings.Join(wordsSl[:i], " ")
 			if comm, ok := commands.List()[commandName]; ok {
 				args = wordsSl[i:]
 				err := comm.Callback(cfg, args...)
 				if err != nil {
-					fmt.Println(err)
+					fmt.Println("Error:", err)
 				}
-				break
+				return
 			}
-			// If command not found
-			commandName = ""
 		}
 
-		if commandName == "" {
-			fmt.Println("Command not found")
-		}
+		fmt.Println("Command not found")
 	}
+}
+
+func emptyCompleter(_ prompt.Document) []prompt.Suggest {
+	return []prompt.Suggest{}
 }
 
 func commFormatter(words string) []string {
 	if !strings.Contains(words, "get") {
 		words = strings.ToLower(words)
 	}
-	wordsSlice := strings.Fields(words)
-
-	return wordsSlice
+	return strings.Fields(words)
 }
